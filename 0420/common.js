e0d365bf9a06bfa97644442fc14df25bd42d465c
yamada yudai <>
2018-04-20 16:48:32 +0900

    javascript No.1  school

---
+++ 0420/common.js
@@ -0,0 +1,60 @@
+console.log('test')
+
+var num = 10
+var test = 'Hello'
+
+console.log(num)
+console.log(test)
+
+var a = 1
+var b = 2
+a += b // a = a + b
+console.log(a);
+a -= b
+console.log(a);
+
+a++
+console.log(a); // +1
+var c = 1
+c--
+
+console.log(c)
+console.log(10%3) //Amari
+
+
+console.log('Moji', "moji")
+console.log('0')
+
+console.log(114514)
+
+console.log(true, false)
+
+console.log(typeof '0')
+console.log(typeof 0)
+console.log(typeof true)
+
+var d = 0
+var e = 1
+var f = 1
+var g = 2
+console.log(d === e)
+console.log(e === f)
+console.log(e !== f)
+console.log(d > e)
+console.log(d < e)
+console.log(e <= f)
+console.log(e >= f)
+console.log(!(e === f))
+console.log(!(d > f))
+
+var h = d === e
+console.log("h is ", h)
+console.log(!h)
+console.log(typeof h)
+
+var n = 30
+console.log(n > 0 && n < 20);
+console.log(n > 0 || n < 20);
+//test
+/*adada
+adaddawd*/

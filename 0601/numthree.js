1c7f5e0c7eadfaa1f2697d9e6771ca98ea823b54
yamada yudai <>
2018-06-08 13:49:58 +0900

    no message

--- 0601/numthree.js
+++ 0601/numthree.js
@@ -3,14 +3,13 @@
 function cutthree(num1){
   var num = num1.toString()
   numlen = num.length
-  stringdot = ','
-  for(var i = numlen , j = 0; i >= 0; i-- , j++){
-    if(j%3 === 0){
-      marknum = num[i-1]
-      marknum += ','
-      console.log(marknum);
+  var marknum = ''
+  for(var i = numlen  - 1, j = 1; i >= 0; i-- , j++){
+    marknum = num[i] + marknum
+    if(j%3 === 0 && j !== 0){
+      marknum = ',' + marknum;
     }
   }
-
+  return marknum
 }
 console.log(cutthree(1234567890));

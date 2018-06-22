5d8b460be5157e1ef195f5b43910545ad219f1d1
yamada yudai <>
2018-06-22 16:09:16 +0900

    改善点 １、Back　Nextのクリックされた時の処理の簡略化 ２、Css整備 ３、タブを進み続けた際に、止まる仕様ではなくループさせる仕様へ変更（コメントアウトにて残しておる）

--- 0615/common.js
+++ 0615/common.js
@@ -5,6 +5,7 @@ const prev = $('.prev');
 const next = $('.next')
 
 var currentNum = 0;
+move(currentNum);
 
 tabMenuItem.on('click', function(){
   move($(this).index())
@@ -12,22 +13,24 @@ tabMenuItem.on('click', function(){
 
 prev.on('click', function(){
   console.log('prev');
-  currentNum--;
-  move(currentNum)
+  move(currentNum-1)
 })
 next.on('click', function(){
   console.log('next');
-  currentNum++;
-  move(currentNum)
+  move(currentNum+1)
 })
 
 function move(num){
-  num = Math.min(tabMenuItem.length - 1, num);
-  num = Math.max(0, num);
+  //num = Math.min(tabMenuItem.length - 1, num);
+  //num = Math.max(0, num);
+  var AllItemNum = tabMenuItem.length - 1
+  num = num > AllItemNum ? 0 : num ;
+  num = num < 0 ? AllItemNum : num ;
   tabMenuItem.removeClass('on')
   tabItem.removeClass('on')
   tabMenuItem.eq(num).addClass('on')
   tabItem.eq(num).addClass('on')
+
   currentNum = num;
   console.log(currentNum);
 }

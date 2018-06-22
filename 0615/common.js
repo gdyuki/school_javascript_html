1083f88d29888be57f392f01225cbb75bf26ad5d
yamada yudai <>
2018-06-22 17:02:33 +0900

    その後の変更 １、すべての変数をローカル変数へ ２、タブを複製

--- 0615/common.js
+++ 0615/common.js
@@ -1,36 +1,47 @@
+function tab(option){
+    const tabMenuItem = option.el.find('.tabmenuItem')
+    const tabItem = option.el.find('.tabItem')
+    const prev = option.el.find('.prev');
+    const next = option.el.find('.next')
 
-const tabMenuItem = $('.tabmenuItem')
-const tabItem = $('.tabItem')
-const prev = $('.prev');
-const next = $('.next')
+    var currentNum = 0;
+    move(currentNum);
 
-var currentNum = 0;
-move(currentNum);
+    tabMenuItem.on('click', function(){
+      move($(this).index())
+    })
 
-tabMenuItem.on('click', function(){
-  move($(this).index())
-})
+    prev.on('click', function(){
+      console.log('prev');
+      move(currentNum-1)
+    })
+    next.on('click', function(){
+      console.log('next');
+      move(currentNum+1)
+    })
 
-prev.on('click', function(){
-  console.log('prev');
-  move(currentNum-1)
-})
-next.on('click', function(){
-  console.log('next');
-  move(currentNum+1)
-})
+    function move(num){
+      var AllItemNum = tabMenuItem.length - 1
+      if(option.isRoop){
+        num = num > AllItemNum ? 0 : num ;
+        num = num < 0 ? AllItemNum : num ;
+      }else {
+        num = Math.min(tabMenuItem.length - 1, num);
+        num = Math.max(0, num);
+      }
+      tabMenuItem.removeClass('on')
+      tabItem.removeClass('on')
+      tabMenuItem.eq(num).addClass('on')
+      tabItem.eq(num).addClass('on')
 
-function move(num){
-  //num = Math.min(tabMenuItem.length - 1, num);
-  //num = Math.max(0, num);
-  var AllItemNum = tabMenuItem.length - 1
-  num = num > AllItemNum ? 0 : num ;
-  num = num < 0 ? AllItemNum : num ;
-  tabMenuItem.removeClass('on')
-  tabItem.removeClass('on')
-  tabMenuItem.eq(num).addClass('on')
-  tabItem.eq(num).addClass('on')
-
-  currentNum = num;
-  console.log(currentNum);
+      currentNum = num;
+      console.log(currentNum);
+    }
 }
+tab({
+  el: $('.tab1'),
+  isRoop: true
+})
+tab({
+  el: $('.tab2'),
+})

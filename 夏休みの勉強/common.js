ea13dcc97a47fae9340c12b32e8fc6b34a0d0c4a
yamada <>
2018-09-14 17:07:13 +0900

    no message

---
+++ 夏休みの勉強/common.js
@@ -0,0 +1,12 @@
+var long_sign = document.getElementsByClassName('.clock_m');
+var short_sign = document.getElementsByClassName('.clock_h');
+var long_img = document.getElementById('#clock_m_img');
+var long_img = document.getElementById('#clock_h_img');
+
+function clock_work(){
+  var time = new Date();
+
+  console.log(time);
+}
+
+clock_work();
